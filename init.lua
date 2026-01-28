d385b2bf632a4dcb6a2bbc9c3117ee72106b614c
RemuSalminen <remumooses.salminen@gmail.com>
2026-01-29 01:17:58 +0200

    Disable lazydev in blink.cmp; Gave errors in non-lua files

--- init.lua
+++ init.lua
@@ -80,14 +80,14 @@ require('lze').load {
 			},
 			fuzzy = {},
 			sources = {
-				default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-				providers = {
-					lazydev = {
-						name = "LazyDev",
-						module = "lazydev.integrations.blink",
-						score_offset = 100,
-					},
-				},
+				default = { "lsp", "path", "snippets", "buffer", "omni" },
+				--providers = {
+				--	lazydev = {
+				--		name = "LazyDev",
+				--		module = "lazydev.integrations.blink",
+				--		score_offset = 100,
+				--	},
+				--},
 			},
 		})
 		end,
