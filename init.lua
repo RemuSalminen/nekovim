165236393961e2787c51d1dee9f6ddeeca057cde
RemuSalminen <remumooses.salminen@gmail.com>
2026-03-18 01:00:44 +0200

    Hide Statusline on Dashboard

--- init.lua
+++ init.lua
@@ -391,6 +391,15 @@ nixInfo.lze.load {
       }
 
       require('alpha').setup(opts)
+
+      -- Hide Statusline
+      vim.api.nvim_create_autocmd({ "User" }, {
+        pattern = { "AlphaReady" },
+        callback = function()
+          vim.cmd([[ set laststatus=0 | autocmd BufUnload <buffer> set laststatus=3 ]])
+        end,
+      })
+
     end,
   },
   {
